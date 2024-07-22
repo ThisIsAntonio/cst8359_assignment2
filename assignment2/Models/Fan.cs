76fab108439d80af34912ab1c778969486d4003b
Marcos Astudillo <antonio.ac86@hotmail.com>
2024-07-22 12:26:47 -0400

    finished create models

---
+++ assignment2/Models/Fan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment2.Models
+{
+    public class Fan
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        public string FullName => LastName + ", " + FirstName;
+
+        public ICollection<Subscription> Subscriptions { get; set; }
+    }
+}
---
+++ assignment2/Models/SportClub.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment2.Models
+{
+    public class SportClub
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Registration Number")]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Fee { get; set; }
+
+        public ICollection<Subscription> Subscriptions { get; set; }
+    }
+}
